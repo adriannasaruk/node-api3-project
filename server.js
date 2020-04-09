2fa5b4ca71ce16a71cbc4991470e6b79a913351d
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-04-08 14:51:42 -1000

    finished project

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 
+const userRouter = require("./users/userRouter.js")
+const postRouter = require("./posts/postRouter.js")
+
 const server = express();
+
+server.use(logger)
+
+server.use(express.json());
+
+server.use("/api/users", userRouter)
+server.use("/api/posts", postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl} ${Date.now()}`);
+
+  next();
+}
 
 module.exports = server;
