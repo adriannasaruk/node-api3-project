90884e939d93bbfec8bdea9aa491dca1252c22a9
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-04-09 09:09:56 -1000

    updated project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const server = require('./server.js');
 
-server.listen(5001, () => {
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
   console.log('\n* Server Running on http://localhost:5001 *\n');
 });
